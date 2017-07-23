c473aaa94087670dde168bfced7cd644cdb9aebb
Michael <mchen0358@gmail.com>
2017-07-23 10:26:13 +0700

    min build to 16 (4.1)

---
+++ base/src/main/java/com/project/michael/base/models/Pagination.java
@@ -0,0 +1,11 @@
+package com.project.michael.base.models;
+
+/**
+ * Created by michael on 7/21/17.
+ */
+
+public class Pagination {
+    public String next;
+    public String prev;
+    public int Total;
+}
