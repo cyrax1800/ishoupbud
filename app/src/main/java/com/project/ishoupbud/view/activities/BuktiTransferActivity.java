2387042d9a9eae050e13b101db4e037249c4db3d
Michael <mchen0358@gmail.com>
2017-07-23 17:57:44 +0700

    fix select photo image uri problem

--- app/src/main/java/com/project/ishoupbud/view/activities/BuktiTransferActivity.java
+++ app/src/main/java/com/project/ishoupbud/view/activities/BuktiTransferActivity.java
@@ -1,7 +1,9 @@
 package com.project.ishoupbud.view.activities;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -303,8 +305,34 @@ public class BuktiTransferActivity extends BaseActivity {
         }else if(requestCode == SELECT_FILE){
             if(resultCode == SUCCESS){
                 imageUri = data.getData();
-                mCurrentPhotoPath = imageUri.getLastPathSegment().replace("file://","");
+                mCurrentPhotoPath = getRealPathFromUri(this, imageUri);
+                if(mCurrentPhotoPath == null){
+                    if(imageUri.getLastPathSegment().startsWith("file")){
+                        mCurrentPhotoPath = imageUri.getLastPathSegment().replace("file://","");
+                    }else{
+                        mCurrentPhotoPath = imageUri.getPath();
+                    }
+                }
                 processImage();
+            }
+        }
+    }
+
+    public String getRealPathFromUri(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }catch (IllegalArgumentException e) {
+            return null;
+        }catch (NullPointerException e){
+            return null;
+        }finally {
+            if (cursor != null) {
+                cursor.close();
             }
         }
     }
--- app/src/main/java/com/project/ishoupbud/view/activities/EditProfileActivity.java
+++ app/src/main/java/com/project/ishoupbud/view/activities/EditProfileActivity.java
@@ -1,7 +1,9 @@
 package com.project.ishoupbud.view.activities;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -287,8 +289,38 @@ public class EditProfileActivity extends BaseActivity {
             if(resultCode == SUCCESS){
                 isChangePicture = true;
                 imageUri = data.getData();
-                mCurrentPhotoPath = imageUri.getLastPathSegment().replace("file://","");
+//                Log.d(TAG, "onActivityResult: " + imageUri.toString());
+//                Log.d(TAG, "onActivityResult: " + imageUri.getPath());
+//                Log.d(TAG, "onActivityResult: " + getRealPathFromUri(this, imageUri));
+                mCurrentPhotoPath = getRealPathFromUri(this, imageUri);
+                if(mCurrentPhotoPath == null){
+                    if(imageUri.getLastPathSegment().startsWith("file")){
+                        mCurrentPhotoPath = imageUri.getLastPathSegment().replace("file://","");
+                    }else{
+                        mCurrentPhotoPath = imageUri.getPath();
+                    }
+                }
+//                Log.d(TAG, "onActivityResult: " + mCurrentPhotoPath);
                 processImage();
+            }
+        }
+    }
+
+    public String getRealPathFromUri(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }catch (IllegalArgumentException e) {
+            return null;
+        }catch (NullPointerException e){
+            return null;
+        }finally {
+            if (cursor != null) {
+                cursor.close();
             }
         }
     }
@@ -326,9 +358,9 @@ public class EditProfileActivity extends BaseActivity {
                 .crossFade()
                 .skipMemoryCache(true)
                 .into(ivProfile);
-        Log.d(TAG, "processImage: " + encodedImage );
-        Log.d(TAG, "processImage: " + file.length() );
-        Log.d(TAG, "processImage: " + encodedImage.length() );
+//        Log.d(TAG, "processImage: " + encodedImage );
+//        Log.d(TAG, "processImage: " + file.length() );
+//        Log.d(TAG, "processImage: " + encodedImage.length() );
     }
 
     private File createImageFile() throws IOException {
